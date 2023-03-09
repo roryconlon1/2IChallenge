d817056dbecb1383b23f432cb9b7fec9c9df8f44
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2023-03-09 12:04:04 +0000

    added tests

--- src/main/java/CodeChallenge.java
+++ src/main/java/CodeChallenge.java
@@ -2,7 +2,11 @@ import java.util.Arrays;
 
 public class CodeChallenge {
 
-    public static int simpleCheck(int a, int b){
+    public static int simpleCheck(int a, int b) {
         return a * b;
+    }
+
+    public static int checkAddition(int a, int b, int c) {
+        return a + b + c;
     }
 }
--- src/test/java/CodeChallengeTest.java
+++ src/test/java/CodeChallengeTest.java
@@ -5,7 +5,12 @@ import static org.junit.Assert.assertEquals;
 public class CodeChallengeTest {
 
     @Test
-    public void testSorting() {
+    public void testMultiply() {
         assertEquals(20, CodeChallenge.simpleCheck(4, 5));
+    }
+
+    @Test
+    public void testAddition() {
+        assertEquals(19, CodeChallenge.checkAddition(9, 4, 6));
     }
 }
