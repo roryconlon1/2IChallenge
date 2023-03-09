5b4b534023efcaba34114a9b957428bd678c2380
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2023-03-09 11:55:03 +0000

    Initial commit

---
+++ src/main/java/CodeChallenge.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class CodeChallenge {
+
+    public static int simpleCheck(int a, int b){
+        return a * b;
+    }
+}
---
+++ src/test/java/CodeChallengeTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CodeChallengeTest {
+
+    @Test
+    public void testSorting() {
+        assertEquals(20, CodeChallenge.simpleCheck(4, 5));
+    }
+}
