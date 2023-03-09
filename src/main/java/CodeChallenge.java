89c4c0a9185c5dd64ebcceaeedce7c29ceac130d
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2023-03-09 13:54:29 +0000

    revised solution

--- src/main/java/CodeChallenge.java
+++ src/main/java/CodeChallenge.java
@@ -5,12 +5,18 @@ import java.util.Set;
 
 public class CodeChallenge {
 
+//    To solve this solution my thinking was to use a Set to remove duplicates then use the sort and Collections
+//    methods to return a unique list in descending order.
+
+//    I initially had it as one method performing both actions however refactored to make the removing of
+//    duplicates its own method, therefore making it more reusable for example if returning a List with
+//    duplicates removed in ascending order etc.
+
     public static Integer[] uniqueNumbers(Integer[] nums) {
 //        Use a Set to get unique values and therefore remove any duplicates and
 //        convert the set with duplicates removed back to Integer []
         Set<Integer> numsSet = new HashSet<>(Arrays.asList(nums));
-        Integer[] removedDuplicates = numsSet.toArray(new Integer[numsSet.size()]);
-        return removedDuplicates;
+        return numsSet.toArray(new Integer[0]);
     }
 
     public static Integer[] uniqueNumbersDesc(Integer[] nums) {
